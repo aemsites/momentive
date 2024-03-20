9f640ce36e253c6790f99b751a4cfaec987d5b7c
Anuj Mahajan <anujmaha@adobe.com>
2024-03-20 23:16:42 +0530

    refactoring and adding handling for desktop view

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -17,72 +17,7 @@ function toggleMenu(nav, navSections, forceExpanded = null) {
   navSections.setAttribute('aria-expanded', expanded ? 'false' : 'true');
 }
 
-function modifyNavListItemsForMobileView(nav) {
-  if (isDesktop.matches) return;
-
-  const mobileNavExists = nav.getAttribute('mobile-nav');
-  if (mobileNavExists) return;
-  const productsSubList = nav.querySelector('.nav-sections .default-content-wrapper > ul > li > ul');
-  const listItems = productsSubList.querySelectorAll('li');
-
-  // Add classes based on the text content of the list items
-  listItems.forEach((item) => {
-    // Create a new i element and set its class to "arrow right"
-    const arrowElement = document.createElement('i');
-    arrowElement.className = 'arrow-right';
-    if (item.textContent.includes('PRODUCT CATEGORIES')) {
-      item.classList.add('secondary-nav-header', 'secondary-nav-product-categories');
-      item.appendChild(arrowElement);
-    } else if (item.textContent.includes('INDUSTRIES')) {
-      item.classList.add('secondary-nav-header', 'secondary-nav-industries');
-      item.appendChild(arrowElement);
-    } else if (item.textContent.includes('BRAND')) {
-      item.classList.add('secondary-nav-header', 'secondary-nav-brand');
-      item.appendChild(arrowElement);
-    }
-  });
-
-  const productsSubListHTML = Array.from(productsSubList.children).map(child => child.outerHTML).join('');
-  const productsListItem = nav.querySelector('.nav-sections .default-content-wrapper > ul > li');
-  productsSubList.remove();
-  productsListItem.insertAdjacentHTML('afterend', productsSubListHTML);
-  nav.setAttribute('mobile-nav', true);
-}
-
-// Restore nav list items on window resize from mobile to desktop
-function restoreNavListItems() {
-  const nav = document.getElementById('nav');
-  if (nav.getAttribute('mobile-nav') === null) return;
-  const navList = nav.querySelector('.nav-sections .default-content-wrapper > ul');
-  if (navList === null) return;
-  const listItems = navList.querySelectorAll('li');
-
-  const productsSubList = document.createElement('ul');
-  // Remove classes based on the text content of the list items
-  listItems.forEach((item) => {
-    if (item.textContent.includes('PRODUCT CATEGORIES')) {
-      item.classList.remove('secondary-nav-header', 'secondary-nav-product-categories');
-      item.removeChild(item.querySelector('.arrow-right'));
-      productsSubList.append(item);
-    } else if (item.textContent.includes('INDUSTRIES')) {
-      item.classList.remove('secondary-nav-header', 'secondary-nav-industries');
-      item.removeChild(item.querySelector('.arrow-right'));
-      productsSubList.append(item);
-    } else if (item.textContent.includes('BRAND')) {
-      item.classList.remove('secondary-nav-header', 'secondary-nav-brand');
-      item.removeChild(item.querySelector('.arrow-right'));
-      productsSubList.append(item);
-    }
-  });
-
-  if (productsSubList.children.length === 0) return;
-
-  const productListItem = nav.querySelector('.nav-sections .default-content-wrapper > ul > li');
-  productListItem.appendChild(productsSubList);
-  nav.removeAttribute('mobile-nav');
-}
-
-function createTranslateMenu(nav, brandRight) {
+function createTranslateMenu() {
   const translateMenu = document.createElement('div');
   translateMenu.classList.add('translate-menu');
 
@@ -102,7 +37,7 @@ function createTranslateMenu(nav, brandRight) {
     const li = document.createElement('li');
     const a = document.createElement('a');
     a.dataset.url = 'null';
-    a.onclick = () => fnChangeLanguage(language);
+    // a.onclick = () => fnChangeLanguage(language);
     a.textContent = languageNames[index];
     li.append(a);
     languagesDialog.append(li);
@@ -110,12 +45,12 @@ function createTranslateMenu(nav, brandRight) {
 
   translateIcon.addEventListener('click', () => {
     if (languagesDialog.style.display === 'none') {
-        languagesDialog.style.display = 'block';
-        languagesDialog.style.position = 'fixed'; // dialog overlay
-      } else {
-        languagesDialog.style.display = 'none';
-        languagesDialog.style.position = 'static';
-      }
+      languagesDialog.style.display = 'block';
+      languagesDialog.style.position = 'fixed'; // dialog overlay
+    } else {
+      languagesDialog.style.display = 'none';
+      languagesDialog.style.position = 'static';
+    }
   });
 
   translateMenu.append(translateIcon, languagesDialog);
@@ -164,7 +99,7 @@ function decorateNavBrandBar(nav) {
   hamburgerIcon.classList.add('nav-hamburger-icon');
   hamburgerIcon.addEventListener('click', () => {
     const navSections = nav.querySelector('.nav-sections');
-    modifyNavListItemsForMobileView(nav);
+    createPrimaryNavForMobView(nav);
     addEventsToNavItems(nav);
     toggleMenu(nav, navSections)
   });
@@ -175,21 +110,78 @@ function decorateNavBrandBar(nav) {
   return navBrand;
 }
 
-function handleNavListItemClick(item, listItems) {
+function createPrimaryNavForMobView(nav) {
+  if (isDesktop.matches) return;
+
+  const mobileNavExists = nav.getAttribute('mobile-nav');
+  if (mobileNavExists) return;
+  const productsSubList = nav.querySelector('.nav-sections .default-content-wrapper > ul > li > ul');
+  const listItems = productsSubList.querySelectorAll('li');
+
+  // Add classes based on the text content of the list items
+  listItems.forEach((item) => {
+    // Create a new i element and set its class to "arrow right"
+    const arrowElement = document.createElement('i');
+    arrowElement.className = 'arrow-right';
+    if (item.textContent.includes('PRODUCT CATEGORIES')) {
+      item.classList.add('secondary-nav-header', 'secondary-nav-product-categories');
+      item.appendChild(arrowElement);
+    } else if (item.textContent.includes('INDUSTRIES')) {
+      item.classList.add('secondary-nav-header', 'secondary-nav-industries');
+      item.appendChild(arrowElement);
+    } else if (item.textContent.includes('BRAND')) {
+      item.classList.add('secondary-nav-header', 'secondary-nav-brand');
+      item.appendChild(arrowElement);
+    }
+  });
+
+  const productsSubListHTML = Array.from(productsSubList.children).map((child) => child.outerHTML).join('');
+  const productsListItem = nav.querySelector('.nav-sections .default-content-wrapper > ul > li');
+  productsListItem.setAttribute('aria-expanded', 'true');
+  productsSubList.remove();
+  productsListItem.insertAdjacentHTML('afterend', productsSubListHTML);
+  nav.setAttribute('mobile-nav', true);
+}
+
+// Restore nav list items on window resize from mobile to desktop
+function restoreNavOnWindowResize() {
+  const nav = document.getElementById('nav');
+  if (nav.getAttribute('mobile-nav') === null) return;
+  const navList = nav.querySelector('.nav-sections .default-content-wrapper > ul');
+  if (navList === null) return;
+  const listItems = navList.querySelectorAll('li');
+
+  const productsSubList = document.createElement('ul');
+  // Remove classes based on the text content of the list items
+  listItems.forEach((item) => {
+    if (item.textContent.includes('PRODUCT CATEGORIES')) {
+      item.classList.remove('secondary-nav-header', 'secondary-nav-product-categories');
+      item.removeChild(item.querySelector('.arrow-right'));
+      productsSubList.append(item);
+    } else if (item.textContent.includes('INDUSTRIES')) {
+      item.classList.remove('secondary-nav-header', 'secondary-nav-industries');
+      item.removeChild(item.querySelector('.arrow-right'));
+      productsSubList.append(item);
+    } else if (item.textContent.includes('BRAND')) {
+      item.classList.remove('secondary-nav-header', 'secondary-nav-brand');
+      item.removeChild(item.querySelector('.arrow-right'));
+      productsSubList.append(item);
+    }
+  });
+
+  if (productsSubList.children.length === 0) return;
+
+  const productListItem = nav.querySelector('.nav-sections .default-content-wrapper > ul > li');
+  productListItem.appendChild(productsSubList);
+  nav.removeAttribute('mobile-nav');
+}
+
+function handleSecondaryNavForMobView(item, listItems) {
   const expanded = item.getAttribute('aria-expanded') === 'true' ? 'false' : 'true';
   item.setAttribute('aria-expanded', expanded);
 
-
   // Get the child list of the clicked list item
   const childList = item.querySelector('ul');
-
-  // add item specific class
-  if (childList !== null) {
-    if (item.textContent.includes('ORDER PRODUCTS')) {
-      const secondLevelList = childList.querySelector('ul');
-      secondLevelList.classList.add('order-products-sub-list');
-    }
-  }
 
   if (expanded === 'true') {
     listItems.forEach((siblingItem) => {
@@ -198,9 +190,12 @@ function handleNavListItemClick(item, listItems) {
       }
     });
     if (childList) {
-      const childListCopy = childList.cloneNode(true);
-      childListCopy.classList.add('sub-nav-list');
-      item.insertAdjacentElement('afterend', childListCopy);
+      childList.classList.add('sub-nav-list');
+      childList.style.display = 'block';
+      if (item.textContent.includes('ORDER PRODUCTS')) {
+        const secondLevelList = childList.querySelector('ul');
+        secondLevelList.classList.add('order-products-sub-list');
+      }
     }
   } else {
     // If 'aria-expanded' is 'false', display all list items
@@ -208,11 +203,28 @@ function handleNavListItemClick(item, listItems) {
       siblingItem.style.display = 'block';
     });
 
-    const adjacentElement = item.nextElementSibling;
-    // If the adjacent element is the copied unordered list, remove it
-    if (adjacentElement && adjacentElement.classList.contains('sub-nav-list')) {
-      adjacentElement.remove();
+    if (childList) {
+      childList.classList.add('sub-nav-list');
+      childList.style.display = 'none';
     }
+  }
+}
+
+function handleSecondaryNavForDesktop(nav, item) {
+  // const secondaryNav = item.querySelector('.secondary-nav');
+  const subList = item.querySelector('ul');
+  const expanded = subList.getAttribute('aria-expanded') === 'true' ? 'false' : 'true';
+  if (expanded === 'false') {
+    subList.setAttribute('aria-expanded', 'false');
+  } else {
+    subList.setAttribute('aria-expanded', 'true');
+    subList.className = 'sub-nav-list';
+		//TODO: specially handle these cases
+    // if (item.textContent.includes('PRODUCT CATEGORIES')) {
+		//
+    // } else if (item.textContent.includes('ORDER PRODUCTS')) {
+		//
+    // }
   }
 }
 
@@ -220,7 +232,13 @@ function addEventsToNavItems(nav) {
   const listItems = nav.querySelectorAll('.nav-sections .default-content-wrapper > ul > li');
 
   listItems.forEach((item) => {
-    item.addEventListener('click', () => handleNavListItemClick(item, listItems));
+    item.addEventListener('click', () => {
+      if (isDesktop.matches) {
+        handleSecondaryNavForDesktop(nav, item);
+      } else {
+        handleSecondaryNavForMobView(item, listItems);
+      }
+    });
   });
 }
 
@@ -248,11 +266,14 @@ export default async function decorate(block) {
     if (section) section.classList.add(`nav-${c}`);
   });
 
+  if (isDesktop.matches) {
+    addEventsToNavItems(nav);
+  }
   window.addEventListener('resize', () => {
     if (window.matchMedia('(max-width: 900px)').matches) {
-      modifyNavListItemsForMobileView(nav);
+      createPrimaryNavForMobView(nav);
     } else {
-      restoreNavListItems();
+      restoreNavOnWindowResize();
     }
   });
 
