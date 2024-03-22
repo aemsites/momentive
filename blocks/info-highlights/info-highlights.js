48ad5491c6df255a1dcda01960f13276dec58fd9
Anurag Gupta <anurgup@adobe.com>
2024-03-22 17:13:06 +0530

    fix linting errors

--- blocks/info-highlights/info-highlights.js
+++ blocks/info-highlights/info-highlights.js
@@ -1,35 +1,36 @@
-const DEFAULT_COLOR = "red";
-
-export default function decorate(block) {
-    // getting color code of block
-    let colorCode = getColorCode(block);
-
-    if (colorCode == "no-color") {
-        block.classList.add(DEFAULT_COLOR);
-        colorCode = DEFAULT_COLOR;
-    }
-
-    // check if "contact-us" need to be appended in the block
-    if (block.classList.contains("contact-us")) {
-        // appending the "contact-us" in the block
-        let contactUsParentBlock = block.firstElementChild.firstElementChild;
-        contactUsParentBlock.innerHTML += 
-                            `<div class="contact-us"> 
-                                <a href="/en-us/contact-us">
-                                    <span class="` + colorCode + `">CONTACT US</span>
-                                </a>
-                            </div>`;    
-    }
-}
+const DEFAULT_COLOR = 'red';
 
 function getColorCode(block) {
-    if (block.classList.contains("blue")) {
-        return "blue-color";
-    } else if (block.classList.contains("red")) {
-        return "red-color";
-    } else if (block.classList.contains("orange")) {
-        return "orange-color";
-    } else {
-        return "no-color";
-    }
+  if (block.classList.contains('blue')) {
+    return 'blue-color';
+  } else if (block.classList.contains('red')) {
+    return 'red-color';
+  } else if (block.classList.contains('orange')) {
+    return 'orange-color';
+  }
+  return 'no-color';
+}
+
+export default function decorate(block) {
+  // getting color code of block
+  let colorCode = getColorCode(block);
+
+  if (colorCode === 'no-color') {
+  block.classList.add(DEFAULT_COLOR);
+  colorCode = DEFAULT_COLOR;
+  }
+
+  // check if "contact-us" need to be appended in the block
+  if (block.classList.contains('contact-us')) {
+
+  // appending the "contact-us" in the block
+  const contactUsParentBlock = block.firstElementChild.firstElementChild;
+
+  contactUsParentBlock.innerHTML += 
+    `<div class="contact-us"> 
+    <a href="/en-us/contact-us">
+      <span class="` + colorCode + `">CONTACT US</span>
+    </a>
+    </div>`;
+  }
 }
