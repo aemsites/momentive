7b0e388a9447322fba8938d459c1c6600230222e
Anurag Gupta <anurgup@adobe.com>
2024-03-22 17:29:24 +0530

    fix linting errors

--- blocks/info-highlights/info-highlights.js
+++ blocks/info-highlights/info-highlights.js
@@ -1,14 +1,15 @@
 const DEFAULT_COLOR = 'red';
 
 function getColorCode(block) {
+  let colorCode = 'no-color';
   if (block.classList.contains('blue')) {
-    return 'blue-color';
+    colorCode = 'blue-color';
   } else if (block.classList.contains('red')) {
-    return 'red-color';
+    colorCode = 'red-color';
   } else if (block.classList.contains('orange')) {
-    return 'orange-color';
+    colorCode = 'orange-color';
   }
-  return 'no-color';
+  return colorCode;
 }
 
 export default function decorate(block) {
@@ -16,21 +17,19 @@ export default function decorate(block) {
   let colorCode = getColorCode(block);
 
   if (colorCode === 'no-color') {
-  block.classList.add(DEFAULT_COLOR);
-  colorCode = DEFAULT_COLOR;
+    block.classList.add(DEFAULT_COLOR);
+    colorCode = DEFAULT_COLOR;
   }
 
   // check if "contact-us" need to be appended in the block
   if (block.classList.contains('contact-us')) {
+    // appending the "contact-us" in the block
+    const contactUsParentBlock = block.firstElementChild.firstElementChild;
 
-  // appending the "contact-us" in the block
-  const contactUsParentBlock = block.firstElementChild.firstElementChild;
-
-  contactUsParentBlock.innerHTML += 
-    `<div class="contact-us"> 
-    <a href="/en-us/contact-us">
-      <span class="` + colorCode + `">CONTACT US</span>
-    </a>
-    </div>`;
+    contactUsParentBlock.innerHTML += `<div class="contact-us"> 
+                                        <a href="/en-us/contact-us">
+                                            <span class="${colorCode}">CONTACT US</span>
+                                        </a>
+                                       </div>`;
   }
 }
