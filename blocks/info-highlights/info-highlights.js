5c0764384ba0db2cbf71c3ff1326a282cede404b
Anurag Gupta <anurgup@adobe.com>
2024-03-22 16:15:23 +0530

    Adding Info Highlights block

---
+++ blocks/info-highlights/info-highlights.js
@@ -0,0 +1,27 @@
+export default function decorate(block) {
+    // check if "contact-us" need to be appended in the block
+    if (block.classList.contains("contact-us")) {
+
+        // getting color code of block
+        let colorCode = getColorCode(block);
+
+        // appending the "contact-us" in the block
+        let contactUsParentBlock = block.firstElementChild.firstElementChild;
+        contactUsParentBlock.innerHTML += 
+                            `<div class="contact-us"> 
+                                <a href="/en-us/contact-us">
+                                    <span class="` + colorCode + `">CONTACT US</span>
+                                </a>
+                            </div>`;    
+    }
+}
+
+function getColorCode(block) {
+    if (block.classList.contains("blue")) {
+        return "blue-color";
+    } else if (block.classList.contains("red")) {
+        return "red-color";
+    } else if (block.classList.contains("orange")) {
+        return "orange-color";
+    }
+}
