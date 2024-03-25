e45658f06147219847e1f3960fef27cc9de3b0fb
Brendan Robert <brobert@adobe.com>
2024-03-25 12:56:11 -0500

    Only show download all if there are multiple pdfs to download

--- blocks/documents/documents.js
+++ blocks/documents/documents.js
@@ -12,28 +12,34 @@ async function decorateTechnicalDocuments(block) {
 
 async function decorateProductDocuments(block) {
   const placeholders = await fetchPlaceholders();
+  let downloadableLinks = 0;
   const documents = [...block.querySelectorAll('a')].map((document) => {
     // check if link ends with pdf
     const isPdf = document.href.endsWith('.pdf');
+
     const icon = createElement('i', { class: `fas fa-file${isPdf ? '-pdf' : ''}` });
     const link = createElement('a', { href: document.href, class: isPdf ? 'show-download' : '' }, icon, document.textContent);
     if (isPdf) {
       link.setAttribute('download', '');
+      downloadableLinks += 1;
     }
     return createElement('div', { class: 'document' }, link);
   });
   const header = createElement('h2', { id: 'product-literature' }, placeholders.productLiterature);
-  const downloadAll = createElement('a', { href: '#', class: 'download-all' }, placeholders.downloadAll);
-  downloadAll.addEventListener('click', (event) => {
-    event.preventDefault();
-    documents.forEach((document) => {
-      const link = document.querySelector('a');
-      if (link.classList.contains('show-download')) {
-        link.click();
-      }
+  if (downloadableLinks > 1) {
+    const downloadAll = createElement('a', { href: '#', class: 'download-all' }, placeholders.downloadAll);
+    downloadAll.addEventListener('click', (event) => {
+      event.preventDefault();
+      documents.forEach((document) => {
+        const link = document.querySelector('a');
+        if (link.classList.contains('show-download')) {
+          link.click();
+        }
+        documents.append(downloadAll);
+      });
     });
-  });
-  return createElement('div', {}, header, createElement('div', { class: 'documents' }, ...documents, downloadAll));
+  }
+  return createElement('div', {}, header, createElement('div', { class: 'documents' }, ...documents));
 }
 
 export default async function decorate(block) {
