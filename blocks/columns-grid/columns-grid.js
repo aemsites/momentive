9ad73e49e8b2b92eda4457b5a799789afadc87a0
anurgup-adobe <anurgup@adobe.com>
2024-04-12 11:21:19 +0530

    adding columns grid - making 4,3,2 evenly distributed columns as default based on count of children

--- blocks/columns-grid/columns-grid.js
+++ blocks/columns-grid/columns-grid.js
@@ -1,6 +1,13 @@
 export default function decorate(block) {
   const cols = [...block.firstElementChild.children];
   block.classList.add(`columns-${cols.length}-cols`);
+  if (cols.length === 3) {
+    block.classList.add('grid33x3');
+  } else if (cols.length === 4) {
+    block.classList.add('grid25x4');
+  } else if (cols.length === 2 && (!block.classList.contains('grid33-66') && !block.classList.contains('grid66-33'))) {
+    block.classList.add('grid50x2');
+  }
 
   [...block.children].forEach((row) => {
     if (row.children.length > 1) {
